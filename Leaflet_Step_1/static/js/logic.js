aba3f426a2d4a9e23e7ddc7dad2590c6a24ce5fb
Nataliia Sokolova <newbeadwork@gmail.com>
2021-02-01 14:12:23 -0800

    First map

--- Leaflet_Step_1/static/js/logic.js
+++ Leaflet_Step_1/static/js/logic.js
@@ -1,12 +1,31 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl).then(function(data) {
-  // The data.features object is in the GeoJSON standard
-  console.log(data.features);
-
-  
-
+var myMap = L.map("mapid", {
+  center: [45.52, -122.67],
+  zoom: 13
 });
 
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+
+d3.json(queryUrl).then(function(data) {
+  
+  console.log(data.features);
+
+  var earthquakes = L.geoJSON(data.features);
+  console.log(earthquakes);
+
+  
+  });
+
+  
