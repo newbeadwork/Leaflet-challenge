fcb5d82baa6c4bca6ea72293e424778c64c5656a
Nataliia Sokolova <newbeadwork@gmail.com>
2021-02-01 15:22:00 -0800

    Basic dark layer and markers

--- Leaflet_Step_1/static/js/logic.js
+++ Leaflet_Step_1/static/js/logic.js
@@ -1,19 +1,4 @@
 
-var myMap = L.map("mapid", {
-  center: [45.52, -122.67],
-  zoom: 13
-});
-
-
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
-
 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
@@ -22,9 +7,26 @@ d3.json(queryUrl).then(function(data) {
   
   console.log(data.features);
 
+  
   var earthquakes = L.geoJSON(data.features);
-  console.log(earthquakes);
 
+  
+
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [darkmap, earthquakes]
+  });
   
   });
 
